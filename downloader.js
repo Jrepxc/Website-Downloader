496e2990cbe11b31a7c2690ccb0aac649d69813b
Jake Priddle <50683048+Jrepxc@users.noreply.github.com>
2020-10-03 06:55:56 -0700

    Update downloader.js

--- downloader.js
+++ downloader.js
@@ -1,6 +1,6 @@
 const scrape = require('website-scraper'); 
 const websiteUrl = 'http://www.google.com/';
- 
+//really easy script i made in 5 minutes after looking at a yt video about web scraping
 scrape({ 
     urls: [websiteUrl], 
     urlFilter: function (url) { 
